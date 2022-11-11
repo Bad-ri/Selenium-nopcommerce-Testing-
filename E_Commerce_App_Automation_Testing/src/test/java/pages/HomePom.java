d7ee5353c6bc18620f99bc3e287e134604532572
20194841 <20194841@FUE.EDU.EG>
2022-11-11 15:51:09 +0200

    Update HomePom.java

--- E_Commerce_App_Automation_Testing/src/test/java/pages/HomePom.java
+++ E_Commerce_App_Automation_Testing/src/test/java/pages/HomePom.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HomePom {
+    // not used
     public WebDriver open_browser(WebDriver driver){
         String path = System.getProperty("user.dir")+"\\src\\main\\resources\\Driver\\chromedriver.exe";
         System.setProperty("webdriver.chrome.driver",path);
